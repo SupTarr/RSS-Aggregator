b2762e16d466deadab5461eb8c7470064d7f0c9f
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-12-03 17:08:02 +0700

    feat: create feeds model

---
+++ sql/schema/003_feeds.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE feeds (
+  id UUID PRIMARY KEY,
+  name TEXT NOT NULL,
+  url TEXT UNIQUE NOT NULL,
+  user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+-- +goose Down
+DROP TABLE feeds;
