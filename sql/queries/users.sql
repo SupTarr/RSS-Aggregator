01e8dc636a09be1dcaee52c891de7adfd58594e5
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-12-03 16:15:49 +0700

    feat: add new field to user: api_key

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,4 +1,8 @@
 -- name: CreateUser :one
-INSERT INTO users (id, name)
-VALUES ($1, $2)
+INSERT INTO users (id, name, api_key)
+VALUES (
+    $1,
+    $2,
+    encode(sha256(random()::text::bytea), 'hex')
+  )
 RETURNING *;
